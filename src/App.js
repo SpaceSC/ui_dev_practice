5b4b5d7739a0fe63be7cdd8a81e653605595b3e9
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-19 16:28:16 +0100

    Import Dreamjob component to App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dreamjob from './components/Dreamjob.component';
+
+function App() {
+  return (
+    <div className="App">
+      <Dreamjob/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Dreamjob.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dreamjob() {
+  return (
+    <div>
+      <h1>Find Your Dream Job With Us</h1>
+    </div>
+  )
+}
+
+export default Dreamjob;
