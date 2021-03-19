b24efc8f4c5d648d02a0a5136672e4548dad082f
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-19 19:32:43 +0100

    Add mobile layout

--- src/components/Menu.component.jsx
+++ src/components/Menu.component.jsx
@@ -1,27 +1,92 @@
-import { IconButton } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import { makeStyles } from '@material-ui/core/styles';
+import { useState } from "react";
+import { IconButton } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    background: '#000',
+    background: "#000",
     border: 0,
-    borderRadius: 25,
-    color: 'white',
-    height: 48,
-    width: 48,
-    padding: '10px',
+    borderRadius: 50,
+    color: "white",
+    padding: "10px",
+    margin: "15px",
+  },
+  hamburger: {
+    fontSize: 60, 
+    "@media screen and (max-width: 570px)": {
+      fontSize: 30,
+    },
+  },
+  drawerHeader: {
+    width: 200,
   },
 }));
 
 function Menu() {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <div >
-      <IconButton className={classes.root}>
-        <MenuIcon />
+    <div>
+      <IconButton className={classes.root} onClick={handleDrawerOpen}>
+        <MenuIcon className={classes.hamburger} />
       </IconButton>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="right"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronRightIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
     </div>
   );
 }
